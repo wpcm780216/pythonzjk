26d1e076639d08bccbd7ef0fb501abbf02ebe9c3
wpcm <wpcm13400468159@live.cn>
2021-03-09 16:52:18 +0800

    wpcm
    Signed-off-by: wpcm <wpcm13400468159@live.cn>

--- vivivvi/Robot.py
+++ vivivvi/Robot.py
@@ -343,8 +343,8 @@ if __name__ == '__main__':
     # 输出路径
     output_path = './wpcm2.yaml'
     # 规则策略
-    config_url = 'https://cdn.jsdelivr.net/gh/Celeter/convert2clash@master/config.yaml'
-    #config_url = 'https://github.com/wpcm780216/Clash/raw/master/custom.clash.yaml'
+    #config_url = 'https://cdn.jsdelivr.net/gh/Celeter/convert2clash@master/config.yaml'
+    config_url = 'https://github.com/wpcm780216/Clash/raw/master/custom.clash.yaml'
     config_path = './config.yaml'
 
     if sub_url is None or sub_url == '':
